881e47789230a0ce7f97dd619aa2a5fdf811eb0d
Alek <vilijam23@gmail.com>
2013-01-20 00:22:12 +0100

    Start new projec

---
+++ I-Logic/Controllers/SiteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace I_Logic.Controllers
+{
+    public class SiteController : Controller
+    {
+        //
+        // GET: /Site/
+        Ilogic db = new Ilogic();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        {
+            Dictionary<string, string> res = new Dictionary<string, string>();
+            var proekti = (from x in db.PROEKTIs where x.Ime == "babba" select x.Ime).FirstOrDefault();
+            res.Add("ime_proekti", proekti);
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
